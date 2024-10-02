ccfa037b1ae7646ddb82381b5468a174abffb4c6
David Kuo <h124079274@msn.com>
2024-10-02 16:11:43 +0800

    fix: issue [沒有用到講者的 SNS]
    * add sns url link

--- 2024/dev/src/components/ModalContentSpeakers/index.js
+++ 2024/dev/src/components/ModalContentSpeakers/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './styles.css';
 
-export default ({ video_url, alt, speakers, position, title, topic, abstract, onClickCloseBtn }) => (
+export default ({ video_url, alt, speakers, position, title, topic, abstract, onClickCloseBtn, sns_url }) => (
   <div className="modal-content-speakers">
     <a className="modal-content-speakers__close-btn" onClick={onClickCloseBtn} >&nbsp;</a>
     <div className="modal-content-speakers__img-section">
-      <img className="modal-content-speakers__img" src={video_url !== null && video_url !== "" ? video_url : "https://raw.githubusercontent.com/iplayground/SessionData/2024/v1/images/speakers/nopic.png"} alt={alt} />
+      <a href={sns_url} target="_blank" rel="noopener noreferrer">
+        <img className="modal-content-speakers__img" src={video_url !== null && video_url !== "" ? video_url : "https://raw.githubusercontent.com/iplayground/SessionData/2024/v1/images/speakers/nopic.png"} alt={alt} />
+      </a>
     </div>
     <div className="modal-content-speakers__txt-section">
       <p className="modal-content-speakers__name"><strong>{speakers[0].name}</strong></p>
