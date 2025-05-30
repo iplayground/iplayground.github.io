4699e8b8e8256d5892819ef3a201301a5d6e39d4
David Kuo <h124079274@msn.com>
2025-05-30 11:37:05 +0800

    update: languageSelector change 'Droidkaigi' style

--- 2025/dev/src/LanguageSelector.js
+++ 2025/dev/src/LanguageSelector.js
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
-import ReactFlagsSelect from 'react-flags-select';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './LanguageSelector.css';
+import { IoLanguage } from "react-icons/io5";
 
 const LanguageSelector = () => {
   const { i18n, t } = useTranslation();
-  const [selected, setSelected] = useState('FIXED');
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+  const [selectLang, setSelectLang] = useState("Language");
 
-  const handleSelect = (code) => {
-    setSelected('FIXED');
-    i18n.changeLanguage(code.toLowerCase());
+  const toggleMenu = () => setOpen((prev) => !prev);
+
+  const handleChange = (lang) => {
+    i18n.changeLanguage(lang.toLowerCase());
+    setSelectLang(lang);
+    setOpen(false);
   };
 
-  const customLabels = {
-    TW: t('lang.zh'),
-    US: t('lang.en'),
-    JP: t('lang.ja'),
-  };
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
 
   return (
-    <div className="language-selector-wrapper">
-      <ReactFlagsSelect
-        selected={selected}
-        onSelect={handleSelect}
-        countries={['TW', 'US', 'JP']}
-        customLabels={customLabels}
-        placeholder={t('lang.placeholder')}
-        className="custom-flags-select"
-      />
+    <div className="lang-selector" ref={menuRef}>
+      <button className='lang-button' onClick={toggleMenu}>
+        <IoLanguage /><span className='lang-code'>{selectLang}</span>
+        </button>
+      {open && (
+        <div className='lang-dropdown'>
+          <button onClick={() => handleChange('TW')}>繁體中文</button>
+          <button onClick={() => handleChange('US')}>English</button>
+          <button onClick={() => handleChange('JP')}>日本語</button>
+        </div>
+      )}
     </div>
   );
 };
