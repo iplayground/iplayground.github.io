2a892d77cabaf796796b0524062426954257c4f1
David Kuo <h124079274@msn.com>
2025-07-25 15:26:44 +0800

    fix: use correct direct url

--- 2025/dev/src/index.js
+++ 2025/dev/src/index.js
@@ -6,9 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import LanguageRouter from './LanguageRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const isLocalhost = window.location.hostname === 'localhost';
+const basename = isLocalhost ? '/' : '/2025';
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/2025">
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<Navigate to="/en" replace />} />
         <Route path="/:lang/*" element={<LanguageRouter />} />
