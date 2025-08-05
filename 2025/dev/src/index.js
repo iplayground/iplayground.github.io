9bc5cbf1a80e82fed209481834a972add2402014
David Kuo <h124079274@msn.com>
2025-08-06 00:41:22 +0800

    fix: use hash router to solve the problem of multi-language URLs - 2

--- 2025/dev/src/index.js
+++ 2025/dev/src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import LanguageRouter from './LanguageRouter';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
 
-const isLocalhost = window.location.hostname === 'localhost';
-const basename = isLocalhost ? '/' : '/2025';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/en" replace />} />
         <Route path="/:lang/*" element={<LanguageRouter />} />
+        <Route path="*" element={<Navigate to="/en" replace />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
